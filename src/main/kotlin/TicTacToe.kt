082c2fcb348f88342490e327ae956beb10dd08f2
Justin Sheldon <JSheldon3488@gmail.com>
2018-10-28 00:52:26 -0500

    Added draw X and O functionality

--- src/main/kotlin/TicTacToe.kt
+++ src/main/kotlin/TicTacToe.kt
@@ -8,6 +8,13 @@ import javafx.scene.layout.StackPane
 import javafx.stage.Stage
 import javafx.scene.shape.Rectangle
 import javafx.scene.paint.Color
+import javafx.scene.text.Font
+import javafx.scene.text.Text
+
+//Right now the only way I can think of changing between X and O on the same click button is to make this a global variable
+// The problem is each square is it's own object so I need them to all share the variable playerTurn if I want to switch
+// between players. (hopefully I can think of cleaner solution later)
+var playerTurn = "X"
 
 class TicTacToeApp: Application(){
     override fun start(primaryStage: Stage) {
@@ -33,12 +40,28 @@ class TicTacToeApp: Application(){
 
     //Used to create the Tic Tac Toe squares inside of the main window
     private class TTTSquare: StackPane() {
+        var text = Text()
+
         init {
             val border = Rectangle(200.0,200.0)
             border.fill = null
             border.stroke = Color.BLACK
             setAlignment(Pos.CENTER)
-            children.add(border)
+            text.font = Font.font(88.0)
+            children.addAll(border, text)
+            //Sets the call on the Node itself instead of needing X and Y coordinates
+            setOnMouseClicked { draw() }
+        }
+
+        fun draw() {
+            if (playerTurn == "X"){
+                text.text = "X"
+                playerTurn = "O"
+            }
+            else {
+                text.text = "O"
+                playerTurn = "X"
+            }
         }
     }
 }
